d3f4426a5f78a480b64cf1356b1660f628fa6add
Kim-Anh Le Cao <k.lecao@uq.edu.au>
2010-02-24 00:00:00 +0000

    version 2.55

---
+++ R/f.R
@@ -0,0 +1,8 @@
+
+
+"f" <-
+function(){
+cat("This package has been superseded by the package mixOmics. integrOmics will not be supported anymore")
+}
+
+
---
+++ R/zzz.R
@@ -0,0 +1,5 @@
+.First.lib<- function(lib, integrOmics){
+cat("This package has been superseded by the package mixOmics.", '\n', " integrOmics will not be supported anymore.")
+}
+
+
